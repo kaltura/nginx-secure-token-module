0c95a2e8da6fb30f8c0d85a8f9cfce7c222c1055
KUROISHI Mitsuo <kuroishi@iij.ad.jp>
2019-03-06 11:03:00 +0900

    rename path to acl and use 'complex value' for it.

--- ngx_http_secure_token_filter_module.h
+++ ngx_http_secure_token_filter_module.h
@@ -7,6 +7,8 @@
 // functions
 ngx_int_t ngx_http_secure_token_get_acl(ngx_http_request_t *r, ngx_http_complex_value_t *acl_conf, ngx_str_t* acl);
 
+ngx_int_t ngx_http_secure_token_get_acl_iijpta(ngx_http_request_t *r, ngx_http_complex_value_t *acl_conf, ngx_str_t* acl);
+
 // globals
 extern ngx_module_t ngx_http_secure_token_filter_module;
 
