47102265e0de15b251c189630be9d61f73c36493
erankor <eran.kornblau@kaltura.com>
2022-11-20 21:27:08 +0200

    m3u8: add part tag names to list

--- ngx_http_secure_token_m3u8.c
+++ ngx_http_secure_token_m3u8.c
@@ -4,7 +4,9 @@
 static ngx_str_t uri_tags[] = {
 	ngx_string("EXT-X-MAP"),
 	ngx_string("EXT-X-KEY"),
+	ngx_string("EXT-X-PART"),
 	ngx_string("EXT-X-MEDIA"),
+	ngx_string("EXT-X-PRELOAD-HINT"),
 	ngx_string("EXT-X-I-FRAME-STREAM-INF"),
 	ngx_null_string,
 };
